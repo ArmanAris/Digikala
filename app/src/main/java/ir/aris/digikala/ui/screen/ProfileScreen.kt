52ac073c1e712260787e7e5a59619617ec9f1779
ArmanAris <114592462+ArmanAris@users.noreply.github.com>
2023-03-04 18:37:06 +0330

    master: multi language

--- app/src/main/java/ir/aris/digikala/ui/screen/ProfileScreen.kt
+++ app/src/main/java/ir/aris/digikala/ui/screen/ProfileScreen.kt
@@ -1,11 +1,62 @@
 package ir.aris.digikala.ui.screen
 
 
+import android.app.Activity
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import ir.aris.digikala.MainActivity
+import ir.aris.digikala.util.Constants.ENGLISH_LANG
+import ir.aris.digikala.util.Constants.PERSIAN_LANG
+import ir.aris.digikala.viewmodel.DataStoreViewModel
 
 @Composable
-fun ProfileScreen(navController: NavHostController) {
-    Text(text = "ProfileScreen")
+fun ProfileScreen(
+    navController: NavHostController,
+    dataStore: DataStoreViewModel = hiltViewModel(),
+) {
+
+    val activity = LocalContext.current as Activity
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Blue),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "ProfileScreen")
+        Button(onClick = {
+            dataStore.saveUserLanguage(PERSIAN_LANG)
+            //  activity.finish()
+            //  activity.startActivity(Intent(activity, MainActivity::class.java))
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+
+        }) {
+            Text(text = "fa")
+        }
+        Button(onClick = {
+            dataStore.saveUserLanguage(ENGLISH_LANG)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }) {
+            Text(text = "en")
+        }
+    }
+
 }
